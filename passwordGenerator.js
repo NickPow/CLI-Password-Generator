77b4787f88505c7826febe3a1a6dc6b83556411e
Nicholas Power <nicholas-power@outlook.com>
2025-01-23 21:49:15 -0330

    3rd commit: Added options for uppercase, and symbols. Added comments, and cleaned up code.

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,20 +1,27 @@
 // Function to generate password
-function generatePassword(length, includeNumbers) {
-    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    const numbers = '0123456789';
-  
-    let characters = lowercase;
-  
-    if (includeNumbers) characters += numbers;
-  
-    let password = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      password += characters[randomIndex];
-    }
-  
-    return password;
+function generatePassword(
+  length,
+  includeNumbers,
+  includeUppercase,
+  includeSymbols
+) {
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  let characters = lowercase;
+  if (includeNumbers) characters += numbers;
+  if (includeUppercase) characters += uppercase;
+  if (includeSymbols) characters += symbols;
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
   }
-  
-  module.exports = { generatePassword };
-  
+
+  return password;
+}
+
+module.exports = { generatePassword };
